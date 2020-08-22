00a9d9f794ba598c5ebeddf891b636615fe5a5f6
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-08-22 21:26:50 +0200

    runner

--- ProductivityTools.PSPublishNugetToGallery/ProductivityTools.PSPublishNugetToGallery.psm1
+++ ProductivityTools.PSPublishNugetToGallery/ProductivityTools.PSPublishNugetToGallery.psm1
@@ -1,6 +1,9 @@
-<#
-	My Function
-#>
-function Get-Function {
+function Publish-NugetToGallery {
+	
+	[Cmdletbinding()]
+	param()
 
+	Write-Verbose "Hello from Publish-NugetToGallery"
 }
+
+Export-ModuleMember Publish-NugetToGallery
---
+++ ProductivityTools.PSPublishNugetToGallery/Runner.ps1
@@ -0,0 +1,4 @@
+clear
+cd $PSScriptRoot
+Import-Module .\ProductivityTools.PSPublishNugetToGallery.psm1
+Publish-NugetToGallery -Verbose
