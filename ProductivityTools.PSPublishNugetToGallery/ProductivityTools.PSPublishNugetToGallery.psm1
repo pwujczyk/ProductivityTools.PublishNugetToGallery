ac77256f5971c049a7020fe042c6b6e4577d981e
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-08-22 21:58:52 +0200

    body method

--- ProductivityTools.PSPublishNugetToGallery/ProductivityTools.PSPublishNugetToGallery.psm1
+++ ProductivityTools.PSPublishNugetToGallery/ProductivityTools.PSPublishNugetToGallery.psm1
@@ -1,9 +1,21 @@
 function Publish-NugetToGallery {
 	
 	[Cmdletbinding()]
-	param()
+	param(
+		[string]$Path=$(Get-Location)
+	)
 
 	Write-Verbose "Hello from Publish-NugetToGallery"
+	Write-Verbose $Path
+
+	dotnet pack
+	
+	$nugetApiKey=Get-MasterConfiguration "nugetApiKey"
+	$nugets=Get-ChildItem *.nuget -Recurse
+	foreach($nuget in $nugets)
+	{
+		dotnet nuget push -s https://api.nuget.org/v3/index.json -k $nugetApiKey $nuget
+	}
 }
 
 Export-ModuleMember Publish-NugetToGallery
--- ProductivityTools.PSPublishNugetToGallery/Runner.ps1
+++ ProductivityTools.PSPublishNugetToGallery/Runner.ps1
@@ -1,4 +1,4 @@
 clear
 cd $PSScriptRoot
-Import-Module .\ProductivityTools.PSPublishNugetToGallery.psm1
-Publish-NugetToGallery -Verbose
+Import-Module .\ProductivityTools.PSPublishNugetToGallery.psm1 -Force 
+Publish-NugetToGallery -Verbose -Path "D:\GitHub\ProductivityTools.Purchase.Contract"
