2e7b871bfa809d25e0c3252e9fb6c59c9fe1918f
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-09-24 23:04:04 +0200

    runner

--- ProductivityTools.PSPublishNugetToGallery/Runner.ps1
+++ ProductivityTools.PSPublishNugetToGallery/Runner.ps1
@@ -2,6 +2,6 @@ clear
 cd $PSScriptRoot
 Import-Module .\ProductivityTools.PSPublishNugetToGallery.psm1 -Force 
 Get-ChildItem
-cd d:\GitHub\ProductivityTools.BankAccounts\ProductivityTools.BankAccounts.Contract\
+cd d:\GitHub\ProductivityTools.BankAccounts.Contract\
 
-Publish-NugetToGallery 
+Publish-NugetToGallery  -Verbose
