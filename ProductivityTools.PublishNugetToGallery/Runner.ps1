d896abba9777ad2664ec1023a6bc19e27dfa3b97
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-11-16 20:31:05 +0100

    rename project

--- ProductivityTools.PublishNugetToGallery/Runner.ps1
+++ ProductivityTools.PublishNugetToGallery/Runner.ps1
@@ -1,6 +1,6 @@
 clear
 cd $PSScriptRoot
-Import-Module .\ProductivityTools.PSPublishNugetToGallery.psm1 -Force 
+Import-Module .\ProductivityTools.PublishNugetToGallery.psm1 -Force 
 Get-ChildItem
 cd d:\GitHub\ProductivityTools.BankAccounts.Contract\
 cd  D:\GitHub-3.PublishedToLinkedIn\ProductivityTools.NetworkUtilities
